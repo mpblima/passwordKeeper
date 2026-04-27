331cc2fa9ace819d37d5b247746d40ac359150a4
Marcelo Lima <mpblima@gmail.com>
2026-04-27 15:30:50 -0300

    Improving the password sharing

--- src/services/googleDrive.ts
+++ src/services/googleDrive.ts
@@ -34,8 +34,7 @@ import {
 } from "../config";
 
 const SCOPES = [
-  "https://www.googleapis.com/auth/drive.file",
-  "https://www.googleapis.com/auth/drive.readonly",
+  "https://www.googleapis.com/auth/drive",
   "https://www.googleapis.com/auth/userinfo.email",
 ].join(" ");
 
